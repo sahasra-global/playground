5ac1419509211f10c09f6ce9859d75af200e202f
Coding Lakshmi <lbk.pachipulusu@gmail.com>
2024-12-03 10:41:07 -0800

    Added to README and modifed the .py and .js files, now they are all set up for me to code the answers to the problems

---
+++ python/set03/set03.py
@@ -0,0 +1,11 @@
+# Print the first 10 Fibonacci numbers without recursion
+
+# Create a function that will find the nth Fibonacci number using recursion
+
+# Create a function that will return a Boolean specifying if a number is prime
+
+# Calculate the sum of digits of a positive integer number
+
+# Print the first 100 prime numbers
+
+# Create a function that will return in an array the first “p” prime numbers greater than “n”
