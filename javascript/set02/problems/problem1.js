f2ee9c34f9aaf52afdaaa894e0472502fb226c64
lasyavenna <73162393+lasyavenna@users.noreply.github.com>
2024-12-03 13:35:10 -0800

    Update problem1.js

--- javascript/set02/problems/problem1.js
+++ javascript/set02/problems/problem1.js
@@ -1,7 +1,3 @@
-<<<<<<< HEAD
 //convert temperature from celcius to farenheit
 let c = Number(prompt("Enter a temperature in celcius: "));
 console.log((9*c)/5 +32);
-=======
-let c = prompt("Enter a temperature in celcius: ")
->>>>>>> 2223d718757db078f680b9d9b3529ec78e7fd09d
