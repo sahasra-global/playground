513d52510741250f1e2cc1871c4027b805876233
Coding Lakshmi <lbk.pachipulusu@gmail.com>
2024-12-02 22:18:46 -0800

    Finished all of the problems in set01 python except for this one: Print all the multiplication tables with numbers from 1 to 10.

---
+++ python/set01/set01.py
@@ -0,0 +1,48 @@
+# Print numbers from 1 to 10
+# For Loop
+for i in range(10):
+    print(i)
+
+# While Loop
+count = 1
+while count <= 10:
+    print(count)
+    count += 1
+    
+print() # New Line
+
+# Print the odd numbers less than 100
+oddNumber = 1
+while (oddNumber < 100):
+    if oddNumber % 2 == 1:
+        print(oddNumber)
+    oddNumber += 1
+
+# Print the multiplication table with 7
+num = 1
+multiple = 1
+while (num <= 10):
+    # converting int's to strings, to make it easy to print
+    multiple = 7 * num
+
+    # Printing the multiplcation tables in format
+    print (f"7 * {num} = {multiple}") 
+    num += 1
+
+
+# Print all the multiplication tables with numbers from 1 to 10
+num = 1
+multiple = 1
+while (num <= 10):
+    # converting int's to strings, to make it easy to print
+    multiple = 7 * num
+
+    # Printing the multiplcation tables in format
+    print (f"7 * {num} = {multiple}") 
+    num += 1
+
+# Calculate the sum of numbers from 1 to 10
+
+# Calculate 10!
+
+# Calculate the sum of even numbers greater than 10 and less than 30
