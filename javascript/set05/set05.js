d587ecf87d66f158ab858fac26b08bbf407b3cd4
Coding Lakshmi <lbk.pachipulusu@gmail.com>
2024-12-16 12:55:12 -0800

    finish all problems in set05 javascript except for one

--- javascript/set05/set05.js
+++ javascript/set05/set05.js
@@ -15,6 +15,10 @@ function mergingTwoArrays(array1, array2) {
 function randomlyAssignArrayValues(array1, array2) {
     const randomNumber = getRandomInt(0, 2);
 
+    function getRandomInt(min, max) {
+        return Math.floor(Math.random() * (max - min)) + min;
+    }
+
     if (randomNumber === 0) {
         return array1;
     }
@@ -22,13 +26,43 @@ function randomlyAssignArrayValues(array1, array2) {
     return array2;
 }
 
-function getRandomInt(min, max) {
-  return Math.floor(Math.random() * (max - min)) + min;
-}
-
 // Create a function that will receive two arrays and will return an array with elements 
 // that are in the first array but not in the second
-
+function returnFirstArray(array1, array2) {
+    return array1
+}
 
 // Create a function that will receive an array of numbers as argument and will return a new array with distinct elements
+function generateArray(size) {
+    
+}
+
 // Calculate the sum of first 100 prime numbers and return them in an array
+function sumOfPrimeNumbersTill100() {
+    let prime = [];
+
+    // Function to check if a number is prime
+    function isPrime(num) {
+        if (num <= 1) return false;
+        for (let i = 2; i <= Math.sqrt(num); i++) {
+            if (num % i === 0) return false;
+        }
+        return true;
+    }
+
+    // Adding all prime numbers into the prime array
+    const max = 100;
+    for (let i = 2; i <= max; i++) {
+        if (isPrime(i)) {
+            prime.push(i);
+        }
+    }
+
+    // Adding all the prime numbers in the prime array. To get the sum of all prime numbers. 
+    let total;
+    for (let i = 0; i < prime.length; i++) {
+        total += prime[i];
+    }
+
+    console.log(total); // logging the total sum of prime numbers to the console. 
+}
