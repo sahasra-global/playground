5dfb0797ae595c32d5308bf177b2938b26a59a46
Coding Lakshmi <lbk.pachipulusu@gmail.com>
2024-12-16 13:50:54 -0800

    finish 2/6 problems in this set. Only in the JS file. Add readME to both py and js set folders.

--- javascript/set03/README.md
+++ javascript/set03/README.md
@@ -1,6 +1,10 @@
-Print the first 10 Fibonacci numbers without recursion
-Create a function that will find the nth Fibonacci number using recursion
-Create a function that will return a Boolean specifying if a number is prime
-Calculate the sum of digits of a positive integer number
-Print the first 100 prime numbers
-Create a function that will return in an array the first “p” prime numbers greater than “n”
+<h1>Set 03 | Lakshmi | JavaScript</h1>
+<h3>Problems solved in the files: </h3>
+<ul>
+    <li>Print the first 10 Fibonacci numbers without recursion</li>
+    <li>Create a function that will find the nth Fibonacci number using recursion</li>
+    <li>Create a function that will return a Boolean specifying if a number is prime</li>
+    <li>Calculate the sum of digits of a positive integer number</li>
+    <li>Print the first 100 prime numbers</li>
+    <li>Create a function that will return in an array the first “p” prime numbers greater than “n”</li>
+</ul>
--- javascript/set03/set03.js
+++ javascript/set03/set03.js
@@ -1,12 +1,53 @@
 // Print the first 10 Fibonacci numbers without recursion
-
+function printingFirstTenFibonacciNumbers() {
+    
+}
 
 // Create a function that will find the nth Fibonacci number using recursion
 
 // Create a function that will return a Boolean specifying if a number is prime
+function isPrime(num) {
+    if (num <= 1) return false;
+    for (let i = 2; i <= Math.sqrt(num); i++) {
+        if (num % i === 0) return false;
+    }
+    return true;
+}
+
+// Testing Problem 3
+console.log(isPrime(5))
+console.log(isPrime(88))
+console.log(isPrime(345))
 
 // Calculate the sum of digits of a positive integer number
 
+
 // Print the first 100 prime numbers
+function printingFirst100PrimeNumbers() {
+    let prime = [];
+
+    // Function to check if a number is prime
+    function isPrime(num) {
+        if (num <= 1) return false;
+        for (let i = 2; i <= Math.sqrt(num); i++) {
+            if (num % i === 0) return false;
+        }
+        return true;
+    }
+
+    // Adding all prime numbers into the prime array
+    const max = 100;
+    for (let i = 2; i <= max; i++) {
+        if (isPrime(i)) {
+            prime.push(i);
+        }
+    }
+
+    // Printing the prime array
+    console.log(prime)
+}
+
+// Testing Problem 5
+printingFirst100PrimeNumbers()
 
 // Create a function that will return in an array the first “p” prime numbers greater than “n”
