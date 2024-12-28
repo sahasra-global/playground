9054f5fa082eba2456ea1f0c5dde4806506861a0
Coding Lakshmi <lbk.pachipulusu@gmail.com>
2024-12-27 18:56:42 -0800

    finished set07 js problems

--- javascript/set07/set07.js
+++ javascript/set07/set07.js
@@ -51,10 +51,47 @@ console.log(convertingStringToASCII(s))
 
 //////////////////////////////////////////////////////////////////////////////////////////
 
-// Create a function that will convert an array containing ASCII codes in a string
+function asciiArrayToString(asciiArray) {
+    return String.fromCharCode(...asciiArray);
+}
+
+// Problem 5: Testing
+const asciiArray = [72, 101, 108, 108, 111];
+console.log(asciiArrayToString(asciiArray)); // Output: "Hello"
 
 //////////////////////////////////////////////////////////////////////////////////////////
-// Implement the Caesar cypher
+
+function caesarCipher(str, shift) {
+    return str.split('').map(char => {
+        const code = char.charCodeAt(0);
+        if (code >= 65 && code <= 90) { // Uppercase letters
+            return String.fromCharCode(((code - 65 + shift) % 26) + 65);
+        } else if (code >= 97 && code <= 122) { // Lowercase letters
+            return String.fromCharCode(((code - 97 + shift) % 26) + 97);
+        } else {
+            return char;
+        }
+    }).join('');
+}
+
+// Problem 6: Testing
+console.log(caesarCipher("Hello World", 3)); // Output: "Khoor Zruog"
 
 //////////////////////////////////////////////////////////////////////////////////////////
-// Implement the bubble sort algorithm for an array of numbers
+
+function bubbleSort(arr) {
+    let n = arr.length;
+    for (let i = 0; i < n - 1; i++) {
+        for (let j = 0; j < n - i - 1; j++) {
+            if (arr[j] > arr[j + 1]) {
+                // Swap arr[j] and arr[j+1]
+                [arr[j], arr[j + 1]] = [arr[j + 1], arr[j]];
+            }
+        }
+    }
+    return arr;
+}
+
+// Problem 7: Testing
+const array = [64, 34, 25, 12, 22, 11, 90];
+console.log(bubbleSort(array)); // Output: [11, 12, 22, 25, 34, 64, 90]
