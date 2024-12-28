567e275336ee38cc0f725c3954a7f707b81f56d7
Coding Lakshmi <lbk.pachipulusu@gmail.com>
2024-12-27 18:58:08 -0800

    finished set07 py problems

--- python/set07/set07.py
+++ python/set07/set07.py
@@ -47,10 +47,41 @@ s = "This is a test of string manipulation"
 print(convertingStringToASCII(s))
 
 ######################################################################################
-# Create a function that will convert an array containing ASCII codes in a string
+
+def ascii_array_to_string(ascii_array):
+    return ''.join(chr(code) for code in ascii_array)
+
+# Problem 5: Testing
+ascii_array = [72, 101, 108, 108, 111]
+print(ascii_array_to_string(ascii_array))  # Output: "Hello"
 
 ######################################################################################
-# Implement the Caesar cypher
+
+def caesar_cipher(text, shift):
+    result = []
+    for char in text:
+        if char.isupper():
+            result.append(chr((ord(char) - 65 + shift) % 26 + 65))
+        elif char.islower():
+            result.append(chr((ord(char) - 97 + shift) % 26 + 97))
+        else:
+            result.append(char)
+    return ''.join(result)
+
+# Problem 6: Testing
+print(caesar_cipher("Hello World", 3))  # Output: "Khoor Zruog"
 
 ######################################################################################
-# Implement the bubble sort algorithm for an array of numbers
+
+def bubble_sort(arr):
+    n = len(arr)
+    for i in range(n):
+        for j in range(0, n - i - 1):
+            if arr[j] > arr[j + 1]:
+                # Swap arr[j] and arr[j + 1]
+                arr[j], arr[j + 1] = arr[j + 1], arr[j]
+    return arr
+
+# Problem 7: Testing
+array = [64, 34, 25, 12, 22, 11, 90]
+print(bubble_sort(array))  # Output: [11, 12, 22, 25, 34, 64, 90]
